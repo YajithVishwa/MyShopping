7338c6ea4e495cd3fb40a749aae4f7284aeaae80
YajithVishwa <yajith@student.tce.edu>
2020-12-20 20:11:33 +0530

    first

--- app/src/main/java/com/yajith/myshopping/DetailActivity.java
+++ app/src/main/java/com/yajith/myshopping/DetailActivity.java
@@ -1,42 +1,31 @@
 package com.yajith.myshopping;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class DetailActivity extends AppCompatActivity {
-    private EditText count,size1,size2,size3;
-    private TextInputLayout t1,t2,t3,t4;
+    private ImageView imageView;
     private Button pay;
     private String type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        count=findViewById(R.id.count);
-        size1=findViewById(R.id.size1);
-        size2=findViewById(R.id.size2);
-        size3=findViewById(R.id.size3);
-        t1=findViewById(R.id.outlinedTextField1);
-        t2=findViewById(R.id.outlinedTextField2);
-        t3=findViewById(R.id.outlinedTextField3);
-        t4=findViewById(R.id.outlinedTextField4);
+        imageView=findViewById(R.id.upload);
         pay=findViewById(R.id.submit);
             type=getIntent().getExtras().getString("type");
-        switch (type)
-        {
-            case "tshirt":t1.setHint("Shirt Count");t2.setHint("Waist Size");t3.setHint("Shoulder Size");t4.setHint("Shirt Length");break;
-            case "uniform":t1.setHint("Uniform Count");t2.setHint("Waist Size");t3.setHint("Shoulder Size");t4.setHint("Uniform Length");break;
-            case "pant":t1.setHint("Pant Count");t2.setHint("Hip Size");t3.setHint("Leg Size");t4.setHint("Pant Length");break;
-            case "trousers":t1.setHint("Trousers Count");t2.setHint("Hip Size");t3.setHint("Thigh Size");t4.setHint("Trouser Length");break;
-        }
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,7 +34,34 @@ public class DetailActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 100);
+            }
+        });
 
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode)
+        {
+            case 100:
+                if(resultCode==RESULT_OK)
+                {
+                    Uri uri=data.getData();
+                    if(uri!=null)
+                    {
+                        Toast.makeText(this, "Upload Success!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
 
     }
 }
--- app/src/main/java/com/yajith/myshopping/SplashActivity.java
+++ app/src/main/java/com/yajith/myshopping/SplashActivity.java
@@ -17,6 +17,6 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
             }
-        },1000);
+        },2000);
     }
 }
